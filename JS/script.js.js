72256c0e659a0b76e0690fa297fba9969625233d
LoganX17 <126016277+LoganX17@users.noreply.github.com>
2023-06-08 21:11:43 +0100

    to do

--- JS/script.js.js
+++ JS/script.js.js
@@ -1,6 +1,112 @@
-$("#save").on("click",function() {
-  alert("Form Submitted!");
-});
+
+// Define the data to be used for the chart
+const data = [
+  { week: 'One', confidence: 75 },
+  { week: 'Two', confidence: 80 },
+  { week: 'Three', confidence: 60 },
+  { week: 'Four', confidence: 65 },
+  { week: 'Five', confidence: 65 },
+  { week: 'Six', confidence: 90 },
+  { week: 'Seven', confidence: 75 },
+  { week: 'Eight', confidence: 80 },
+  { week: 'Nine', confidence: 85 },
+  { week: 'Ten', confidence: 88 }
+];
+
+// Define variables for the chart dimensions and scales
+const margin = { top: 20, right: 20, bottom: 60, left: 60 };
+const width = 800 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
+
+// Create a band scale for the x-axis
+const x = d3.scaleBand()
+  .domain(data.map(d => d.week))
+  .range([0, width])
+  .padding(0.2);
+
+
+// Create a linear scale for the y-axis
+const y = d3.scaleLinear()
+  .domain([0, d3.max(data, d => d.confidence)])
+  .range([height, 150]);
+
+  // Create an SVG element for the chart, define its width and height, and append a group element to it
+const svg = d3.select('#chart')
+  .append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom + 20)
+  .append('g')
+  .attr('transform', `translate(${margin.left},${margin.top})`);
+
+// Create rectangles for each data point and add interactivity with mouseover and mouseout events
+
+svg.selectAll('.bar')
+  .data(data)
+  .enter()
+  .append('rect')
+  .attr('class', 'bar')
+  .attr('x', d => x(d.week))
+  .attr('width', x.bandwidth())
+  .transition()
+  .duration(10000)
+  .attr('y', d => y(d.confidence))  
+  .attr('height', d => height - y(d.confidence))
+  .attr('fill', 'steelblue')
+  .on("mouseover", function (d) {
+    d3.select(this).style("fill", "#5db0f5");
+  })
+  .on("mouseout", function (d) {
+    d3.select(this).style("fill", "steelblue");
+  });
+
+
+
+  // Add x-axis labels to the chart
+svg.append('g')
+  .attr('transform', `translate(0,${height})`)
+  .call(d3.axisBottom(x))
+  .selectAll('text')
+  .style('text-anchor', 'end')
+  .attr('dx', '-.8em')
+  .attr('dy', '.15em')
+  .attr('transform', 'rotate(-65)');
+
+
+
+
+// Add y-axis labels to the chart
+svg.append('g')
+  .call(d3.axisLeft(y));
+
+
+    // Select container element and append an SVG with a height and width
+/*const transitionChartElement = d3
+.select('#transition-easing')
+.append('svg')
+.attr('width', 900)
+.attr('height', 200);*/
+
+
+
+
+// Create the circle
+/*const transitionChartSVG = transitionChartElement
+.append("circle")
+.attr("cx", 100)
+.attr("cy", 50)
+.attr("r", 20)
+.style("fill", "red")
+
+
+// Animate the circle
+transitionChartSVG
+.transition()
+.duration(10000)
+.attr("cx", 800)
+.style("fill", "blue")
+
+// Add easing
+.ease(d3.easeBounceOut)*/
 
 
 /* CODIO SOLUTION BEGIN*/
@@ -10,9 +116,9 @@ const modeStatus = document.querySelector('.mode-status');
 function toggleMode() {
 body.classList.toggle('dark-mode');
 
-const modeMessage = body.classList.contains('dark-mode') ?
+const modeMessage = body.classList.contains('dark-mode');
     'Dark'
-    : "Light"
+     "Light"
 
 modeStatus.innerText = " " + modeMessage;
   }
@@ -74,6 +180,8 @@ function pause(){
 // in this case it will call my timeLoop function every 100 miliseconds
 // this is often enough that a user won't notice the delay, but infrequent enough to not cause the application to get stuck just doing this
 setInterval(timeLoop, 100);
+
+
 
 
 
